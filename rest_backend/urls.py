e1dc74a1ef32cd4299eeee7a1600aec63156ba05
wuyue <wuyue92tree@163.com>
2018-04-22 11:35:32 +0800

    重定向到api-docs

--- rest_backend/urls.py
+++ rest_backend/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/api-docs')),
     path('', include('rest_backend.libs.backend.urls'))
 ]
 
